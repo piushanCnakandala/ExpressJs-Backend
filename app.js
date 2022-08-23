eb570cf5ca1b9c79511438030cc5af81a02c221c
piushanCnakandala <piushancnakandala2000@gmail.com>
2022-08-24 01:03:43 +0530

    update

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 4000
+const customer=require('./routes/customer')
+
+app.use(express.json());   //if we use json object inside our app.js
+
+app.use('/customer',customer)
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
