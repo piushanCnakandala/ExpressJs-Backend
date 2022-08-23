9d15d01e637a52affa698e5482fe9f551961d711
piushanCnakandala <piushancnakandala2000@gmail.com>
2022-08-24 02:34:13 +0530

    connected to the mysql server and item table created

--- routes/item.js
+++ routes/item.js
@@ -5,5 +5,23 @@ const mysql=require('mysql')
 const db=require('../configs/db.configs')
 const connection =mysql.createConnection(db.database)
 
+connection.connect(function (err){
+    if (err){
+        console.log(err)
+    }else {
+        console.log('connected to the mysql server')
+        var tableQuery="CREATE TABLE IF NOT EXISTS item (id VARCHAR(255) PRIMARY KEY,name VARCHAR(255),qty INT, price DOUBLE )"
+        connection.query(tableQuery,function (err,result){
+            if (err)throw  err;
+            if (result.warningCount ==0){
+                console.log("Item Table Created!");
+            }
+        })
+
+    }
+})
+
+
+
 
 module.exports=router
