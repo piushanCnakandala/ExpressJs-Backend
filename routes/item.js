aa6fa00cc3dd96cf13c191ba9892669aa1a7558c
piushanCnakandala <piushancnakandala2000@gmail.com>
2022-08-24 03:11:01 +0530

    items search  done

--- routes/item.js
+++ routes/item.js
@@ -77,4 +77,14 @@ router.delete('/:id',(req,res)=>{
     })
 })
 
+router.get('/:id',(req,res)=>{
+    const id=req.params.id
+
+    var query ='SELECT * FROM item WHERE id=?';
+    connection.query(query,[id],(err,rows) =>{
+        if(err)throw err;
+        res.send(rows)
+    })
+})
+
 module.exports=router
