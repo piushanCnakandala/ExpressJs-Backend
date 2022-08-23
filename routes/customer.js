a8d6657261a00510a88b041d7cbeb8764105b381
piushanCnakandala <piushancnakandala2000@gmail.com>
2022-08-24 01:19:02 +0530

    connected to the mysql server and created customer table

--- routes/customer.js
+++ routes/customer.js
@@ -1,8 +1,25 @@
 const express =require('express')
 const router =express.Router()
 const app =express()
+const mysql=require('mysql')
+const db=require('../configs/db.configs')
+const connection =mysql.createConnection(db.database)
 
-app.use(express.json());
+connection.connect(function (err){
+    if (err){
+        console.log(err)
+    }else {
+        console.log('connected to the mysql server')
+        var tableQuery="CREATE TABLE IF NOT EXISTS customer (id VARCHAR(255) PRIMARY KEY,name VARCHAR(255),address VARCHAR(255),mobileNo VARCHAR(255))"
+        connection.query(tableQuery,function (err,result){
+            if (err)throw  err;
+            if (result.warningCount ==0){
+                console.log("Customer Table Created!");
+            }
+        })
+
+    }
+})
 
 
 module.exports=router
