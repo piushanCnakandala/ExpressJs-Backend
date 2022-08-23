adba2144aa5077facea6224bb96cbc555fb52877
piushanCnakandala <piushancnakandala2000@gmail.com>
2022-08-24 02:26:22 +0530

    import item.js path

--- app.js
+++ app.js
@@ -2,14 +2,12 @@ const express = require('express')
 const app = express()
 const port = 4000
 const customer=require('./routes/customer')
+const item =require('./routes/item')
 
 app.use(express.json());   //if we use json object inside our app.js
-
 app.use('/customer',customer)
+app.use('/item',item)
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
