e172f0720b31ce40fb124e24d0da5478e0895d1d
piushanCnakandala <piushancnakandala2000@gmail.com>
2022-08-25 00:30:47 +0530

    update

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const port = 4000
 const customer=require('./routes/customer')
 const item =require('./routes/item')
 const order=require('./routes/order')
+const orderDetails=require('./routes/ordersDetails')
 
 app.use(express.json());   //if we use json object inside our app.js
 app.use('/customer',customer)
 app.use('/item',item)
 app.use('/order',order)
+app.use('/details',orderDetails)
 
 
 app.listen(port, () => {
